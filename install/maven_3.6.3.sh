d9147aa3e974caaa1e07f01f30063b27cf499767
月之领主LM <178910432@qq.com>
2021-04-01 01:04:30 +0800

    Update

--- install/maven_3.6.3.sh
+++ install/maven_3.6.3.sh
@@ -13,14 +13,18 @@ mkdir -m 777 -p '/usr/local/maven/'
 rm -rf '/usr/local/maven/apache-maven-3.6.3'
 tar -zxvf 'apache-maven-3.6.3-bin.tar.gz' -C '/usr/local/maven/'
 
-# 手动修改配置文件：/usr/local/maven/apache-maven-3.6.3/conf/settings.xml
-
+# 添加命令行支持
 echo '' >> '/etc/profile'
 echo '# Maven' >> '/etc/profile'
 echo 'MAVEN_HOME=/usr/local/maven/apache-maven-3.6.3' >> '/etc/profile'
 echo 'export PATH=${MAVEN_HOME}/bin:${PATH}' >> '/etc/profile'
 echo '' >> '/etc/profile'
 cat '/etc/profile'
+
+# 修改配置
+mkdir -m 777 -p '/usr/local/maven/repo'
+apache_maven_settings_url='https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz'
+wget "$apache_maven_settings_url" -O '/usr/local/maven/apache-maven-3.6.3/conf/settings.xml'
 
 # OpenJDK 1.8 在线安装
 yum install -y 'java-1.8.0-openjdk-demo.x86_64'
