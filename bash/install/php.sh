64504b8f5dc5ce969d0fd56f19624175577e249d
月之领主LM <178910432@qq.com>
2022-01-20 01:51:53 +0800

    Update

--- bash/install/php.sh
+++ bash/install/php.sh
@@ -23,11 +23,19 @@ fi
 # 开始安装：
 install_software 'php-fpm'
 install_software 'php-mysql'
+install_software 'php-mysqli'
 install_software 'php-pgsql'
-install_software 'php-curl'
+install_software 'php-odbc'
+install_software 'php-redis'
 install_software 'php-memcached'
+install_software 'php-curl'
+install_software 'php-ftp'
+install_software 'php-iconv'
+install_software 'php-json'
+install_software 'php-yaml'
+install_software 'php-mbstring'
 install_software 'php-gd'
-install_software 'php-mcrypt'
+install_software 'php-zip'
 
 php -v
 if [ $? -ne 0 ]; then
