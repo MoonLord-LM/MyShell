be26d7a194348fb6d3f67166238d96e05611b0a3
月之领主LM <178910432@qq.com>
2022-01-21 02:49:22 +0800

    Update

--- bash/web/config.sh
+++ bash/web/config.sh
@@ -18,19 +18,19 @@ fi
 # 配置参数：
 mkdir -p '/etc/nginx/sites-available'
 wget -O '/etc/nginx/sites-available/nginx.conf' --timeout=10 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/web/nginx.conf'
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
     log_error "file can not be created: \"/etc/nginx/sites-available/nginx.conf\", quit now"
     exit 1
 fi
 
 mkdir -p '/etc/nginx/ssl'
 wget -O '/etc/nginx/ssl/server.crt' --timeout=10 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/web/server.crt'
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
     log_error "file can not be created: \"/etc/nginx/ssl/server.crt\", quit now"
     exit 1
 fi
 wget -O '/etc/nginx/ssl/server.key' --timeout=10 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/web/server.key'
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
     log_error "file can not be created: \"/etc/nginx/ssl/server.key\", quit now"
     exit 1
 fi
@@ -40,7 +40,7 @@ rm -rf '/etc/nginx/sites-enabled/default'
 
 mkdir -p '/var/www/php'
 echo '<?php phpinfo(); ?>' > '/var/www/php/index.php'
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
     log_error "file can not be created: \"/var/www/php/index.php\", quit now"
     exit 1
 fi
