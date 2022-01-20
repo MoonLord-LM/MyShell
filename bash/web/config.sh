bf02525a62896ce281efda9c588ebc5db64b1c37
月之领主LM <178910432@qq.com>
2022-01-21 02:17:07 +0800

    Update

--- bash/web/config.sh
+++ bash/web/config.sh
@@ -6,7 +6,10 @@
 
 
 # 配置初始页面
-wget -O '/etc/nginx/sites-available/nginx.conf' --timeout=10 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/web/nginx.conf' 
+wget -O '/etc/nginx/sites-available/nginx.conf' --timeout=10 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/web/nginx.conf'
+wget -O '/etc/nginx/ssl/server.crt' --timeout=10 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/web/server.crt'
+wget -O '/etc/nginx/ssl/server.key' --timeout=10 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/web/server.key'
+
 ln -s '/etc/nginx/sites-available/nginx.conf' '/etc/nginx/sites-enabled/nginx.conf'
 rm -rf '/etc/nginx/sites-enabled/default'
 
