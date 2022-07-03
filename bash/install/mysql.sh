3c1c36de0e7902cae656f39eb6d91109262891d8
月之领主LM <178910432@qq.com>
2022-07-04 07:04:58 +0800

    Update

--- bash/install/mysql.sh
+++ bash/install/mysql.sh
@@ -28,10 +28,14 @@ if [ $? -eq 0 ]; then
     # E: Package 'mysql-server' has no installation candidate
     # http://repo.mysql.com/apt/debian/pool/
 
-    wget -O '/tmp/mysql-apt-config_0.8.22-1_all.deb' --timeout=10 --no-cache \
-    'https://repo.mysql.com/apt/debian/pool/mysql-apt-config/m/mysql-apt-config/mysql-apt-config_0.8.22-1_all.deb'
-    apt install -y '/tmp/mysql-apt-config_0.8.22-1_all.deb'
+    show_software 'mysql-apt-config'
+    if [ $? -ne 0 ]; then
+        wget -O '/tmp/mysql-apt-config_0.8.22-1_all.deb' --timeout=10 --no-cache \
+        'https://repo.mysql.com/apt/debian/pool/mysql-apt-config/m/mysql-apt-config/mysql-apt-config_0.8.22-1_all.deb'
+        apt install -y '/tmp/mysql-apt-config_0.8.22-1_all.deb'
+    fi
 
+    show_software 'mysql-apt-config'
     if [ $? -ne 0 ]; then
         log_error 'mysql-server install failed, quit now'
         exit 1
