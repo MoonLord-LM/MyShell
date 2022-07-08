8a8bbbac948ea44f456d9b764ab161c586c05e20
月之领主LM <178910432@qq.com>
2022-07-09 00:24:33 +0800

    Update

--- bash/install/mysql.sh
+++ bash/install/mysql.sh
@@ -32,8 +32,10 @@ if [ $? -eq 0 ]; then
     if [ $? -ne 0 ]; then
         wget -O '/tmp/mysql-apt-config_0.8.22-1_all.deb' --timeout=10 --no-cache \
         'https://repo.mysql.com/apt/debian/pool/mysql-apt-config/m/mysql-apt-config/mysql-apt-config_0.8.22-1_all.deb'
-        apt install -y '/tmp/mysql-apt-config_0.8.22-1_all.deb'
+
         # 图形界面操作
+        apt install '/tmp/mysql-apt-config_0.8.22-1_all.deb'
+
         show_software 'mysql-apt-config'
         log_info 'mysql-apt-config wait begin'
         sleep 30
