0a1c2c2d7e5626a5f9f44f243c152dcb835a0cc5
月之领主LM <178910432@qq.com>
2022-07-15 23:56:12 +0800

    Update

--- bash/My.sh
+++ bash/My.sh
@@ -334,6 +334,7 @@ function show_tcp_listening(){
 
 # 准备常用的命令
 function prepare_common_command(){
+    check_command_exist 'virt-what' || install_software 'virt-what'
     check_command_exist 'wget' || install_software 'wget'
     check_command_exist 'curl' || install_software 'curl'
     check_command_exist 'git' || install_software 'git'
