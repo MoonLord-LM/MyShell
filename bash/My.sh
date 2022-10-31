0c4354636c910c595fbfb638535ca013414cdd21
MoonLord-LM <me@moonlord.cn>
2022-11-01 01:10:31 +0800

    Update My.sh

--- bash/My.sh
+++ bash/My.sh
@@ -394,7 +394,6 @@ function show_tcp_listening(){
 function prepare_common_command(){
     # common
     check_command_exist 'virt-what' || install_software 'virt-what'
-    check_command_exist 'sshpass' || install_software 'sshpass'
     check_command_exist 'wget' || install_software 'wget'
     check_command_exist 'curl' || install_software 'curl'
     check_command_exist 'git' || install_software 'git'
