899e201517abab61a1086dfd313f72e3ea5e2119
MoonLord-LM <me@moonlord.cn>
2022-11-05 00:36:15 +0800

    Update

--- bash/install/v2ray.sh
+++ bash/install/v2ray.sh
@@ -44,44 +44,6 @@ function v2ray_config_json(){
 EOF
 }
 
-function v2ray_nginx_config(){
-    cat <<EOF
-server {
-
-    listen      80;
-    listen [::]:80;
-
-    listen      443 ssl;
-    listen [::]:443 ssl;
-
-    ssl_certificate     /etc/nginx/ssl/moonlord.cc.crt;
-    ssl_certificate_key /etc/nginx/ssl/moonlord.cc.key;
-
-    server_name ws_10010;
-
-    root /var/www/html;
-    index index.php index.html;
-
-    location /ws/10010 {
-        proxy_pass http://127.0.0.1:10010;
-        proxy_http_version 1.1;
-        proxy_set_header Host $http_host;
-        proxy_set_header Connection "upgrade";
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
-        proxy_redirect off;
-    }
-
-    location ~ \.php$ {
-        include      snippets/fastcgi-php.conf;
-        fastcgi_pass unix:/run/php/php-fpm.sock;
-    }
-
-}
-EOF
-}
-
 
 
 # 加载函数：
@@ -107,7 +69,12 @@ check_command_exist 'nginx'
 if [ $? -eq 0 ]; then
     site_available_path='/etc/nginx/sites-available'
     site_enabled_path='/etc/nginx/sites-enabled'
-    v2ray_nginx_config > "$site_available_path/v2ray.conf"
+    wget -O "$site_available_path/v2ray.conf" --timeout=10 --no-cache "$conf_resource/v2ray.conf"
+    if [ $? -ne 0 ]; then
+        log_error "file create failed: \"$site_available_path/v2ray.conf\", quit now"
+        exit 1
+    fi
+
     rm -rf "$site_enabled_path/v2ray.conf"
     ln -s "$site_available_path/v2ray.conf" "$site_enabled_path/v2ray.conf"
     systemctl restart 'nginx'
