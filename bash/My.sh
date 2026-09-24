594d02388a9c76bb5474a2f4539e21644bb9bd27
MoonLord-LM <me@moonlord.cn>
2026-09-25 04:33:42 +0800

    Update

--- bash/My.sh
+++ bash/My.sh
@@ -408,7 +408,7 @@ function set_tcp_congestion_control_bbr(){
     sysctl 'net.core.default_qdisc'
     sysctl 'net.ipv4.tcp_fastopen'
 }
-# 尝试设置 /usr/memory_swap 文件为虚拟内存，以保证物理内存和虚拟内存的总量在 4GB 以上
+# 尝试设置 /swapfile 文件为虚拟内存，以保证物理内存和虚拟内存的总量在 4GB 以上
 function set_memory_swap_to_4GB(){
     local mem_size=$(free -m | awk '/^Mem:/{print $2}')
     local swap_size=$(free -m | awk '/^Swap:/{print $2}')
@@ -427,7 +427,7 @@ function set_memory_swap_to_4GB(){
     fi
     log_info "set_memory_swap need swap memory: $need_size MB"
 
-    local swap_file='/usr/memory_swap'
+    local swap_file='/swapfile'
     if awk '$2=="file"{print $1}' '/proc/swaps' | grep -q -F "$swap_file"; then
         log_info "set_memory_swap: \"$swap_file\" is active, try to swapoff it first"
         swapoff "$swap_file"
