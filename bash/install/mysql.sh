bf038c8ab5bdbb09bd01ab7dcfbfeb3fa8c1e383
MoonLord-LM <me@moonlord.cn>
2026-09-26 10:32:30 +0800

    Update

--- bash/install/mysql.sh
+++ bash/install/mysql.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# wget -O- --timeout=10 --no-cache 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/install/mysql.sh' | bash
+
 # MySQL
-# 开源地址：https://github.com/mysql/mysql-server
-# 在线安装：wget -O- --timeout=10 --no-cache 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/install/mysql.sh' | bash
+# https://github.com/mysql/mysql-server
 
 
 
-# 参数设置：
+
+
+# ———————————————————————— Config ————————————————————————
 mysql_apt_config_url='https://repo.mysql.com/apt/debian/pool/mysql-apt-config/m/mysql-apt-config/mysql-apt-config_0.8.40-1_all.deb'
 mysql_apt_config_select='mysql-8.4-lts'
 
@@ -37,7 +40,6 @@ EOF
 }
 
 function mysql_gen_ssl_cert(){
-    log_info 'mysql_gen_ssl_cert begin'
     mkdir -p $(dirname "$mysql_ssl_key")
     openssl req -newkey rsa:4096 -nodes -keyout "$mysql_ssl_key" -x509 -days 365000 -out "$mysql_ssl_cert" -subj '/CN=MySQL'
     if [ $? -ne 0 ]; then
@@ -47,11 +49,9 @@ function mysql_gen_ssl_cert(){
 
     chown mysql:mysql "$mysql_ssl_key" "$mysql_ssl_cert"
     chmod 600 "$mysql_ssl_key"
-    log_info 'mysql_gen_ssl_cert ok'
 }
 
 function allow_remote_access(){
-    log_info "allow_remote_access begin"
     mysql -h 'localhost' -u 'root' --batch <<EOF
         create user if not exists '$mysql_user'@'%' identified by '$mysql_password';
         alter user '$mysql_user'@'%' identified by '$mysql_password';
@@ -59,12 +59,13 @@ function allow_remote_access(){
         select user, host, plugin from mysql.user;
         flush privileges;
 EOF
-    log_info "allow_remote_access ok"
 }
 
 
 
-# 加载函数：
+
+
+# ———————————————————————— Init ————————————————————————
 source <( wget -O- --timeout=10 --no-cache 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/My.sh' )
 prepare_common_command
 if [ $? -ne 0 ]; then
@@ -74,10 +75,17 @@ fi
 
 
 
-# 开始安装：
+
+
+# ———————————————————————— Install ————————————————————————
+check_command_exist 'mysqld'
+if [ $? -eq 0 ]; then
+    log_info 'mysql already installed, quit now'
+    exit 0
+fi
+
 show_software 'mysql-apt-config'
 if [ $? -ne 0 ]; then
-    # debconf 预置答案，安装时不会弹出版本选择界面
     echo "mysql-apt-config mysql-apt-config/select-server select $mysql_apt_config_select" | debconf-set-selections
 
     tmp_file="/tmp/mysql-apt-config_${RANDOM}_${RANDOM}_${RANDOM}_${RANDOM}.deb"
@@ -96,7 +104,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-check_command_exist 'mysqld' || install_software 'mysql-community-server'
+install_software 'mysql-community-server'
 mysqld --version
 if [ $? -ne 0 ]; then
     log_error 'mysql-community-server install failed, quit now'
@@ -119,11 +127,11 @@ log_attention "mysql ssl cert: ${mysql_ssl_cert}"
 
 
 
-# 启动服务：
+
+
+# ———————————————————————— Start ————————————————————————
 systemctl restart 'mysql'
 systemctl enable 'mysql'
 systemctl status --no-pager 'mysql'
 
 show_tcp_listening
-
-
