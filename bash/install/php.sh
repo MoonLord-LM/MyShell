2145f75a576ed8e34a358fc2bc218fde42ceea3a
MoonLord-LM <me@moonlord.cn>
2026-09-26 12:22:22 +0800

    Update

--- bash/install/php.sh
+++ bash/install/php.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+# wget -O- --timeout=10 --no-cache 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/install/php.sh' | bash
+
 # PHP
-# 开源地址：https://github.com/php
-# 在线安装：wget -O- --timeout=10 --no-cache 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/install/php.sh' | bash
+# https://github.com/php/php-src
 
 
 
-# 参数设置：
+
+
+# ———————————————————————— Config ————————————————————————
+php_version='8.4'
+php_fpm_service="php${php_version}-fpm"
+php_extensions='opcache mysql pgsql sqlite3 curl mbstring gd bcmath zip redis memcached yaml xml xsl soap intl imagick gmp bz2'
+
+php_apt_repo_url='https://packages.sury.org/php/'
+php_gpg_key_file='/usr/share/keyrings/deb.sury.org-php.gpg'
+php_apt_source_file='/etc/apt/sources.list.d/php.list'
 
 
 
-# 加载函数：
+
+
+# ———————————————————————— Init ————————————————————————
 source <( wget -O- --timeout=10 --no-cache 'https://raw.githubusercontent.com/MoonLord-LM/MyShell/master/bash/My.sh' )
 prepare_common_command
 if [ $? -ne 0 ]; then
@@ -20,54 +32,66 @@ fi
 
 
 
-# 开始安装：
-install_software 'php-cli'
-install_software 'php-fpm'
-install_software 'php-mysql'
-install_software 'php-pgsql'
-install_software 'php-odbc'
-install_software 'php-sqlite3'
-install_software 'php-curl'
-install_software 'php-json'
-install_software 'php-mbstring'
-install_software 'php-gd'
-install_software 'php-bcmath'
-install_software 'php-zip'
-install_software 'php-redis'
-install_software 'php-memcached'
-install_software 'php-yaml'
 
-php -v
+
+# ———————————————————————— Install ————————————————————————
+check_command_exist "php${php_version}"
+if [ $? -eq 0 ]; then
+    log_info 'php already installed, quit now'
+    exit 0
+fi
+
+wget -O "$php_gpg_key_file" --timeout=120 --no-cache "$php_apt_repo_url/apt.gpg"
+if [ $? -ne 0 ]; then
+    log_error 'add sury gpg key failed, quit now'
+    exit 1
+fi
+codename=$(get_system_version_codename)
+echo "deb [signed-by=$php_gpg_key_file] $php_apt_repo_url $codename main" > "$php_apt_source_file"
+
+php_packages="php${php_version}-cli php${php_version}-fpm"
+for php_extension in $php_extensions; do
+    php_packages="${php_packages} php${php_version}-${php_extension}"
+done
+
+update_software
+apt install -y $php_packages
+if [ $? -ne 0 ]; then
+    log_error 'php packages install failed, quit now'
+    exit 1
+fi
+
+php -v | grep --color=never "PHP ${php_version}"
 if [ $? -ne 0 ]; then
     log_error 'php install failed, quit now'
     exit 1
 fi
 
-php_fpm_service_file=$(find '/usr/lib/systemd/system/' -type f 2> '/dev/null' | grep 'php' | grep 'fpm.service$')
-if [ "$php_fpm_service_file" == '' ]; then
+systemctl cat "$php_fpm_service"
+if [ $? -ne 0 ]; then
     log_error 'php-fpm install failed, quit now'
     exit 1
 fi
-log_info "php_fpm_service_file: $php_fpm_service_file"
 
-php_fpm_service=${php_fpm_service_file/#'/usr/lib/systemd/system/'/''}
-php_fpm_service=${php_fpm_service/%'.service'/''}
-if [ "$php_fpm_service" == '' ]; then
-    log_error 'php-fpm install failed, quit now'
+tmp_file="/tmp/composer-setup_${RANDOM}_${RANDOM}_${RANDOM}_${RANDOM}.php"
+wget -O "$tmp_file" --timeout=120 --no-cache 'https://getcomposer.org/installer'
+php "$tmp_file" --install-dir='/usr/local/bin' --filename='composer'
+if [ $? -ne 0 ]; then
+    log_error 'composer install failed, quit now'
     exit 1
 fi
-log_info "php_fpm_service: $php_fpm_service"
+rm -f "$tmp_file"
 
-# Composer
-php -r "copy('https://install.phpcomposer.com/installer', 'composer-setup.php');"
-php 'composer-setup.php' --install-dir=/usr/local/bin --filename=composer
-php -r "unlink('composer-setup.php');"
+log_attention "php version: $(php -r 'echo PHP_VERSION;')"
+log_attention "php extensions: $(php -m | wc -l)"
 
 
 
-# 启动服务：
+
+
+# ———————————————————————— Start ————————————————————————
 systemctl restart "$php_fpm_service"
 systemctl enable "$php_fpm_service"
 systemctl status --no-pager "$php_fpm_service"
 
-
+show_tcp_listening
