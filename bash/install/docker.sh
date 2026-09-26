9d76abf5a96db8925888ef25797af09d37980c36
MoonLord-LM <me@moonlord.cn>
2026-09-27 02:37:51 +0800

    Update

--- bash/install/docker.sh
+++ bash/install/docker.sh
@@ -102,7 +102,7 @@ fi
 log_info 'docker images:' && docker images
 log_info 'docker ps -a:' && docker ps -a
 
-docker image rm 'hello-world'
 docker rm $(docker ps -a -q --filter ancestor=hello-world)
+docker image rm 'hello-world'
 log_info 'docker images:' && docker images
 log_info 'docker ps -a:' && docker ps -a
